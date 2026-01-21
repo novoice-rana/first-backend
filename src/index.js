d14892d1989b526b455901c9c3e20449e695289a
novoice-rana <god0.x.trends@gmail.com>
2026-01-21 22:34:49 +0530

    all source configured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
+import  {app}  from "./app.js";
 
 import connectDB from "./db/index.js";
 
@@ -9,7 +10,7 @@ dotenv.config({
 connectDB()
 .then(()=>[
     app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at port:${process.removeListener.PORT}`);
+        console.log(`Server is running at port:${process.env.PORT}`);
     })
 ])
 .catch((err)=>{
